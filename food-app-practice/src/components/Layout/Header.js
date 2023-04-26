323e9c90b737514f680126788e25820e17014fd6
HojinEun <hojin.eun@lgcns.com>
2023-04-26 09:25:36 +0900

    prac 3 add

---
+++ food-app-practice/src/components/Layout/Header.js
@@ -0,0 +1,22 @@
+import React, { Fragment } from "react";
+
+import HeaderCartButton from "./HeaderCartButton";
+import mealsImage from "../../assets/meals.jpg";
+import classes from "./Header.module.css";
+
+const Header = (props) => {
+  //안에 class 이름에 - 가 들어가는 경우는 div에서 선언한 것과 같이 해야 함.
+  return (
+    <Fragment>
+      <header className={classes.header}>
+        <h1>ReactMeals</h1>
+        <HeaderCartButton onClick={props.onShowCart} />
+      </header>
+      <div className={classes["main-image"]}>
+        <img src={mealsImage} alt="A table full of delicious food!" />
+      </div>
+    </Fragment>
+  );
+};
+
+export default Header;
